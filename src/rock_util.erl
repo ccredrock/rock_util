58994f745143d1f3573b35e30bd40d564224d778
王红岩 <ccredrock@gmail.com>
2017-12-11 19:53:22 +0800

    add to_binary

--- src/rock_util.erl
+++ src/rock_util.erl
@@ -6,7 +6,10 @@
 %%%-------------------------------------------------------------------
 -module(rock_util).
 
--export([start/0, start_time/0, global_uniq/0]).
+-export([start/0]).
+-export([start_time/0,
+         global_uniq/0,
+         to_binary/1]).
 
 %%------------------------------------------------------------------------------
 -spec start() -> {'ok', [atom()]} | {'error', term()}.
@@ -25,4 +28,10 @@ global_uniq() ->
     Start = integer_to_binary(start_time(), 32),
     Uniq = integer_to_binary(erlang:unique_integer([positive, monotonic]), 32),
     <<Start/binary, Uniq/binary>>.
+
+-spec to_binary(binary() | list() | atom() | integer()) -> binary().
+to_binary(X) when is_binary(X) -> X;
+to_binary(X) when is_list(X) -> list_to_binary(X);
+to_binary(X) when is_atom(X) -> atom_to_binary(X, utf8);
+to_binary(X) when is_integer(X) -> integer_to_binary(X).
 
